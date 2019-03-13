da73598d0e78cb3afa80cea8d9f6b4176d89309b
Prashant Sachdeva <sachdevp@cs.ubc.ca>
2019-03-13 11:41:43 -0700

    Handling different types of shell.

--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
-export A4_DEPS=$PWD/deps/
-export GLEW_DIR=$A4_DEPS/glew
-export GLFW_DIR=$A4_DEPS/glfw
-export GLM_INCLUDE_DIR=$A4_DEPS/glm/
+test "$?BASH_VERSION" = "0" || eval 'setenv() { export "$1=$2"; }'
+
+setenv A4_DEPS $PWD/deps/
+setenv GLEW_DIR $A4_DEPS/glew
+setenv GLFW_DIR $A4_DEPS/glfw
+setenv GLM_INCLUDE_DIR $A4_DEPS/glm/
